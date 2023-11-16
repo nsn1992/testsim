6424102da0f97b9f58dbf6f6f94a60a71c02a713
Noe <nsn@usal.es>
2023-11-16 16:31:05 +0100

    Store e.coli genome

---
+++ scripts/pipeline.sh
@@ -0,0 +1,3 @@
+# Firstly, we download the reference ecoli genome from NCBI
+mkdir -p res/genome
+wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
