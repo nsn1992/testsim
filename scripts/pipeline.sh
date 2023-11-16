9cf26e6f943f8cce2f75f33269d44055fa3ff8b5
Noe <nsn@usal.es>
2023-11-16 18:35:32 +0100

    Make an index of the genome

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,3 +1,9 @@
 # Firstly, we download the reference ecoli genome from NCBI
 mkdir -p res/genome
 wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+gunzip res/genome/ecoli.fasta.gz
+# Secondly , we make an index of the genome
+ echo
+    echo "Running STAR index..."
+    mkdir -p res/genome/star_index
+    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
